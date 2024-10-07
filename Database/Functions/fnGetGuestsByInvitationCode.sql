42959c4d135af703464062f0b52efc905b21f667
Thomas Starnes <43146669+tstar98@users.noreply.github.com>
2024-10-06 19:01:44 -0600

    Added fnGetGuestsByInvitationCode SQL function

---
+++ Database/Functions/fnGetGuestsByInvitationCode.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION [dbo].[fnGetGuestsByInvitationCode](@CODE char(5))
+RETURNS @GUESTS TABLE
+(
+	[Id] INT,
+	[FirstName] NVARCHAR(50) NOT NULL,
+	[LastName] NVARCHAR(50) NOT NULL,
+	[Phone] VARCHAR(10) NULL,
+	[Email] VARCHAR(50) NULL,
+	[DietaryRestrictions] NVARCHAR(250) NULL,
+	[RsvpStatus] BIT NOT NULL DEFAULT 0,
+	[RsvpDate] DATETIME NULL
+)
+AS
+BEGIN
+	INSERT @GUESTS
+	SELECT Id, FirstName, LastName, Phone, Email, DietaryRestrictions, RsvpStatus, RsvpDate
+	FROM Guests
+	WHERE InvitationCode = @CODE;
+	RETURN;
+END
